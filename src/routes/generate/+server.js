83c9b48df377343b95b72ca2264a84d4ba2a243c
ariel klevecz <ariel@yaytso.art>
2024-10-18 08:43:33 -0700

    use public

--- src/routes/generate/+server.js
+++ src/routes/generate/+server.js
@@ -1,23 +1,5 @@
 import { REPLICATE_API_TOKEN } from "$env/static/private";
 import { json } from "@sveltejs/kit";
-// {
-//     "id": "qknb76qzznrgm0cjkt9r98vtv0",
-//     "model": "aklevecz/cog-flux-schnell-lora",
-//     "version": "dp-7fa274ea152f4f75b83ecb3a0b1e5f6d",
-//     "input": {
-//         "prompt": "a white cat eating chickens"
-//     },
-//     "logs": "",
-//     "output": null,
-//     "data_removed": false,
-//     "error": null,
-//     "status": "starting",
-//     "created_at": "2024-10-18T07:42:32.445Z",
-//     "urls": {
-//         "cancel": "https://api.replicate.com/v1/predictions/qknb76qzznrgm0cjkt9r98vtv0/cancel",
-//         "get": "https://api.replicate.com/v1/predictions/qknb76qzznrgm0cjkt9r98vtv0"
-//     }
-// }
 
 const headers = {
     'Authorization': `Bearer ${REPLICATE_API_TOKEN}`,
@@ -29,7 +11,7 @@ const headers = {
  * @param {string} prompt - The prompt for the prediction
  * @returns {Promise<ReplicateResponse>}
  */
-async function makeReplicateRequest(prompt) {
+async function makeReplicateRequestPrivate(prompt) {
     const url = 'https://api.replicate.com/v1/deployments/aklevecz/cog-flux-schnell-lora/predictions';
     
     const body = JSON.stringify({
@@ -59,10 +41,48 @@ async function makeReplicateRequest(prompt) {
     }
   }
 
+  /**
+   * @param {string} prompt - The prompt for the prediction
+   * @returns {Promise<ReplicateResponse | undefined>} - The prediction result
+   */
+  const makeReplicateRequestPublic = async (prompt) => {
+    const url = 'https://api.replicate.com/v1/predictions';
+    const headers = {
+      'Authorization': `Bearer ${REPLICATE_API_TOKEN}`,
+      'Content-Type': 'application/json',
+    };
+    const body = JSON.stringify({
+      version: "091495765fa5ef2725a175a57b276ec30dc9d39c22d30410f2ede68a3eab66b3",
+      input: {
+        prompt,
+        hf_lora: "aklevecz/finn_flux"
+      }
+    });
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: body
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
+  
+
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
   const { prompt } = await request.json();
-  const data = await makeReplicateRequest(prompt);
+  const data = await makeReplicateRequestPublic(prompt);
   return json(data);
 }
 
